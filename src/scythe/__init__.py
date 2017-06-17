fed7fa38a34600cb251a31c6043b3ff34ec6ba50
Massimo Santini <massimo.santini@gmail.com>
2017-06-17 10:42:34 +0200

    Cleaned up a lot

--- src/scythe/__init__.py
+++ src/scythe/__init__.py
@@ -1,4 +1,10 @@
+from base64 import decodestring
+import io
 from logging import basicConfig, getLogger, DEBUG, INFO
+from os import chmod, unlink
+from shutil import rmtree
+from tarfile import TarFile
+from tempfile import mkdtemp
 
 from redis import StrictRedis
 
@@ -7,3 +13,26 @@ basicConfig(format = '%(asctime)s %(levelname)s: [%(funcName)s] %(message)s', da
 LOGGER = getLogger(__name__)
 
 redis = StrictRedis(host = 'localhost', port = 6379, db = 0)
+
+def rmrotree(path):
+    def _oe(f, p, e):
+        if p == path: return
+        pp = dirname(p)
+        chmod(pp, 0700)
+        chmod(p, 0700)
+        unlink(p)
+    rmtree(path, onerror = _oe)
+
+def tar2tmpdir(source, decode = False):
+    if decode:
+        fo = io.BytesIO(decodestring(source))
+    else:
+        fo = io.open(source, 'rb')
+    temp_dir = mkdtemp(prefix = 'scythe-', dir = '/tmp' )
+    with TarFile.open(mode = 'r', fileobj = fo) as tf:
+        try:
+            tf.extractall(temp_dir)
+        except IOError:
+            rmrotree(temp_dir)
+            return None
+    return temp_dir
--- src/scythe/config.py
+++ src/scythe/config.py
@@ -1,17 +1,18 @@
-from base64 import decodestring
+from glob import glob
+
 from fnmatch import fnmatch
 import io
 from json import dumps, loads
-from os.path import join, basename, dirname
+from os.path import join, basename, dirname, splitext
 import re
 from tarfile import TarFile
 
 from sf import DEFAULT_ENCODING
 from sf.testcases import TestCase, TestCases
 
-from . import redis, LOGGER
+from . import redis, tar2tmpdir, LOGGER
 
-MARKDOWN_GLOB = '*.md'
+TEXTS_GLOB = '*.md'
 MARKDOWN_RE = re.compile(r'(.*)\.md')
 
 def cases(session_id, exercise):
@@ -29,50 +30,25 @@ def toredis(path, session_id):
         redis.sadd(uids_key, dumps({'uid': uid, 'info': info, 'status': 'registered'}))
         LOGGER.info('Imported uid {}'.format(uid))
 
-    tar_data = io.BytesIO(decodestring(config['TAR_DATA']))
-    cases = {}
-    texts = {}
-    with TarFile.open(mode = 'r', fileobj = tar_data) as tf:
-        for m in tf.getmembers():
-            if m.isfile():
-                exercise_name = dirname(m.name)
-                file_name = basename(m.name)
+    temp_dir = tar2tmpdir(config['TAR_DATA'], decode = True)
 
-                exercise_cases = cases.get(exercise_name, {})
-                for kind in TestCase.KINDS:
-                    if fnmatch(file_name, TestCase.GLOBS[kind]):
-                        case_name = TestCase.TEST_NUM_RE.match(file_name).group(1)
-                        data = tf.extractfile(m).read().decode(DEFAULT_ENCODING)
-                        tc = exercise_cases.get(case_name, TestCase(case_name))
-                        if kind == 'args': data = TestCase.u2args(data)
-                        setattr(tc, kind, data)
-                        exercise_cases[case_name] = tc
-                cases[exercise_name] = exercise_cases
-
-                exercise_texts =  texts.get(exercise_name, {})
-                if fnmatch(file_name, MARKDOWN_GLOB):
-                    markdown_name = MARKDOWN_RE.match(file_name).group(1)
-                    markdown = tf.extractfile(m).read().decode(DEFAULT_ENCODING)
-                    exercise_texts[markdown_name] = markdown
-                texts[exercise_name] = exercise_texts
-
+    cases_key = 'cases:{}'.format(session_id)
     texts_key = 'texts:{}'.format(session_id)
-    for exercise_name, exercise_texts in texts.items():
+
+    for exercise_path in glob(join(temp_dir, '*')):
+
+        exercise_name = basename(exercise_path)
+        list_of_cases = TestCases(exercise_path).to_list_of_dicts(('diffs', 'errors', 'actual'))
+        redis.hset(cases_key, exercise_name, dumps(list_of_cases))
+        LOGGER.info('Imported cases for exercise {}'.format(exercise_name))
+
         list_of_texts = []
-        for text_name, text in exercise_texts.items():
+        for text_path in glob(join(exercise_path, TEXTS_GLOB)):
+            text_name = splitext(basename(text_path))[0]
+            with io.open(text_path, 'r', encoding = DEFAULT_ENCODING) as tf: text = tf.read()
             list_of_texts.append({'name': text_name, 'content': text})
         redis.hset(texts_key, exercise_name, dumps(list_of_texts))
         LOGGER.info('Imported texts for exercise {}'.format(exercise_name))
-
-    cases_key = 'cases:{}'.format(session_id)
-    for exercise_name, exercise_cases in cases.items():
-        list_of_cases = []
-        for case_name, case in exercise_cases.items():
-            dct = case.to_dict()
-            for key in 'diffs', 'actual', 'errors': dct.pop(key, None)
-            list_of_cases.append(dct)
-        redis.hset(cases_key, exercise_name, dumps(list_of_cases))
-        LOGGER.info('Imported cases for exercise {}'.format(exercise_name))
 
 
 if __name__ == '__main__':
