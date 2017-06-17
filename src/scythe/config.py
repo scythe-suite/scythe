c3ac6d3b1a8d5e134215ac07d6bada9be27dab93
Massimo Santini <massimo.santini@gmail.com>
2017-06-17 11:30:28 +0200

    Cleaned up code, due to sim-fun-i updates

--- src/scythe/config.py
+++ src/scythe/config.py
@@ -1,19 +1,14 @@
 from glob import glob
-
-from fnmatch import fnmatch
 import io
 from json import dumps, loads
 from os.path import join, basename, dirname, splitext
-import re
-from tarfile import TarFile
 
 from sf import DEFAULT_ENCODING
-from sf.testcases import TestCase, TestCases
+from sf.testcases import TestCases
 
-from . import redis, tar2tmpdir, LOGGER
+from . import redis, tar2tmpdir, rmrotree, LOGGER
 
 TEXTS_GLOB = '*.md'
-MARKDOWN_RE = re.compile(r'(.*)\.md')
 
 def cases(session_id, exercise):
     cases_key = 'cases:{}'.format(session_id)
@@ -28,7 +23,7 @@ def toredis(path, session_id):
     uids_key = 'uids:{}'.format(session_id)
     for uid, info in config['REGISTERED_UIDS'].items():
         redis.sadd(uids_key, dumps({'uid': uid, 'info': info, 'status': 'registered'}))
-        LOGGER.info('Imported uid {}'.format(uid))
+    LOGGER.info('Imported uids')
 
     temp_dir = tar2tmpdir(config['TAR_DATA'], decode = True)
 
@@ -39,17 +34,24 @@ def toredis(path, session_id):
 
         exercise_name = basename(exercise_path)
         list_of_cases = TestCases(exercise_path).to_list_of_dicts(('diffs', 'errors', 'actual'))
-        redis.hset(cases_key, exercise_name, dumps(list_of_cases))
-        LOGGER.info('Imported cases for exercise {}'.format(exercise_name))
+        if not list_of_cases:
+            LOGGER.warn('Missing cases for {}'.format(exercise_name))
+        else:
+            redis.hset(cases_key, exercise_name, dumps(list_of_cases))
+            LOGGER.info('Imported cases for exercise {}'.format(exercise_name))
 
         list_of_texts = []
         for text_path in glob(join(exercise_path, TEXTS_GLOB)):
             text_name = splitext(basename(text_path))[0]
             with io.open(text_path, 'r', encoding = DEFAULT_ENCODING) as tf: text = tf.read()
             list_of_texts.append({'name': text_name, 'content': text})
-        redis.hset(texts_key, exercise_name, dumps(list_of_texts))
-        LOGGER.info('Imported texts for exercise {}'.format(exercise_name))
+        if not list_of_texts:
+            LOGGER.warn('Missing texts for {}'.format(exercise_name))
+        else:
+            redis.hset(texts_key, exercise_name, dumps(list_of_texts))
+            LOGGER.info('Imported texts for exercise {}'.format(exercise_name))
 
+    rmrotree(temp_dir)
 
 if __name__ == '__main__':
     toredis('confs/170613-t.py','170613')
--- src/scythe/upload.py
+++ src/scythe/upload.py
@@ -1,39 +1,23 @@
-from base64 import decodestring
 from datetime import datetime
 from fnmatch import fnmatch
 from glob import glob
 import io
+
 from json import dumps
-from os import chmod, unlink, symlink, makedirs
 from os.path import join, dirname, basename, isdir, islink
 import re
-from shutil import copytree, rmtree
-from tarfile import TarFile
-from tempfile import mkdtemp
 
 from sf import DEFAULT_ENCODING
 from sf.solution import autodetect_solution, ExecutionException
 from sf.testcases import TestCase, TestCases
 
-from . import redis, LOGGER
+from . import redis, tar2tmpdir, rmrotree, LOGGER
 import config
 
 UID_TIMESTAMP_RE = re.compile( r'.*/(?P<uid>.+)/(?P<timestamp>[0-9]+)\.tar' )
 
 def ts2iso(timestamp):
     return datetime.fromtimestamp(int(timestamp)/1000).isoformat()
-#
-# def json_dump(data, path):
-#     with io.open(path, 'w', encoding = DEFAULT_ENCODING) as f: f.write(dumps(data, ensure_ascii = False, sort_keys = True, indent = 4))
-#
-def rmrotree(path):
-    def _oe(f, p, e):
-        if p == path: return
-        pp = dirname(p)
-        chmod(pp, 0700)
-        chmod(p, 0700)
-        unlink(p)
-    rmtree(path, onerror = _oe)
 
 class Uploads(object):
 
@@ -51,55 +35,43 @@ class Uploads(object):
                 gd = m.groupdict()
                 self.add(gd['uid'], gd['timestamp'])
 
-    def _test(self, temp_dir, uid, timestamp, exercise):
-        exercise_path = join(temp_dir, exercise)
-        cases = config.cases(self.session_id, exercise)
-        if cases is None:
-            LOGGER.warn('Missing cases for {}'.format(exercise))
-            return
-        compile_case = TestCase('<COMPILE>')
-        solution = autodetect_solution(exercise_path)
-        if solution is None:
-            compile_case.errors = u'Missing (or ambiguous) solution'
-            LOGGER.warn('Missing (or ambiguous) solution for {}'.format(exercise))
-        else:
-            compilation_result = solution.compile()
-            if compilation_result.returncode:
-                compile_case.errors = compilation_result.stderr.decode(DEFAULT_ENCODING)
-                LOGGER.warn( 'Failed to compile exercise {}'.format(exercise))
-        result = [compile_case.to_dict()]
-        if not compile_case.errors:
-            LOGGER.info( 'Compiled solution for exercise {}'.format(exercise))
-            num_cases = cases.fill_actual(solution)
-            LOGGER.info( 'Run {} test cases for {}'.format(num_cases, exercise))
-            redis.hset('results:{}:{}'.format(uid, timestamp), exercise, dumps(cases.to_list_of_dicts(('input', 'args', 'expected'))))
-
     def add(self, uid, timestamp):
         redis.zadd(self.timestamps_key.format(uid), timestamp, timestamp)
-        temp_dir = mkdtemp(prefix = 'scythe-', dir = '/tmp' )
+        temp_dir = tar2tmpdir(join(self.path, uid, timestamp + '.tar'))
         LOGGER.info('Processing upload by uid {} at {} (in {})'.format(uid, ts2iso(timestamp), temp_dir))
-        solutions = {}
-        with TarFile.open(join(self.path, uid, timestamp + '.tar'), mode = 'r') as tf:
-            try:
-                tf.extractall(temp_dir)
-            except IOError:
-                LOGGER.error( 'Failed to untar upload for uid {} at timestamp {} ({})'.format(uid, timestamp, ts2iso(timestamp)))
-                exercises = []
-            for m in tf.getmembers():
-                if m.isfile():
-                    exercise_name = dirname(m.name)
-                    file_name = basename(m.name)
-                    exercise_solutions =  solutions.get(exercise_name, {})
-                    solution = tf.extractfile(m).read().decode(DEFAULT_ENCODING)
-                    exercise_solutions[file_name] = solution
-                    solutions[exercise_name] = exercise_solutions
-        solutions_key = 'solutions:{}:{}'.format(uid, timestamp)
-        for exercise_name, exercise_solutions in solutions.items():
-            redis.hset(solutions_key, exercise_name, exercise_solutions)
-            LOGGER.info('Imported solutions for {}'.format(exercise_name))
-            self._test(temp_dir, uid, timestamp, exercise_name)
-        #rmrotree(temp_dir)
 
+        solutions_key = 'solutions:{}:{}'.format(uid, timestamp)
+        compilations_key = 'compilations:{}:{}'.format(uid, timestamp)
+        results_key = 'results:{}:{}'.format(uid, timestamp)
+        for exercise_path in glob(join(temp_dir, '*')):
+            exercise_name = basename(exercise_path)
+
+            solution = autodetect_solution(exercise_path)
+            if solution.sources is None:
+                LOGGER.warn('No solutions found for exercise {}'.format(exercise_name))
+                continue
+            list_of_solutions = []
+            for solution_name in solution.sources:
+                solution_path = join(exercise_path, solution_name)
+                with io.open(solution_path, 'r', encoding = DEFAULT_ENCODING) as tf: solution_content = tf.read()
+                list_of_solutions.append({'name': solution_name, 'content': solution_content})
+            redis.hset(solutions_key, exercise_name, dumps(list_of_solutions))
+            LOGGER.info('Imported solutions for exercise {}'.format(exercise_name))
+            compiler_message = ''
+            if solution.main_source is None:
+                compiler_message = u'Missing (or ambiguous) solution'
+                LOGGER.warn('Missing (or ambiguous) solution for {}'.format(exercise_name))
+            compilation_result = solution.compile()
+            if compilation_result.returncode:
+                compiler_message = compilation_result.stderr.decode(DEFAULT_ENCODING)
+                LOGGER.warn( 'Failed to compile exercise {}'.format(exercise_name))
+            redis.hset(compilations_key, exercise_name, compiler_message)
+            if compiler_message: continue
+            LOGGER.info( 'Compiled solution for exercise {}'.format(exercise_name))
+            cases = config.cases(self.session_id, exercise_name)
+            num_cases = cases.fill_actual(solution)
+            LOGGER.info( 'Run {} test cases for {}'.format(num_cases, exercise_name))
+            redis.hset(results_key, exercise_name, dumps(cases.to_list_of_dicts(('input', 'args', 'expected'))))
 
 if __name__ == '__main__':
     Uploads('harvests/170613/uploads', '170613')
