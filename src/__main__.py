b2e4bc17163f7d96ef093119fd6a1067ba5214d9
Massimo Santini <massimo.santini@gmail.com>
2017-06-19 14:36:53 +0200

    Adding redis stuff

---
+++ src/__main__.py
@@ -0,0 +1,10 @@
+from scythe.cmds import main
+
+if __name__ == '__main__':
+    main()
+
+# from scythe import config, upload
+#
+# if __name__ == '__main__':
+#     config.process('confs/170613-t.py','170613')
+#     upload.scan('harvests/170613/uploads', '170613')
