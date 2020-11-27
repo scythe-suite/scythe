82baa7b0e71043b1849567e3e7613577bb09b3af
Massimo Santini <santini@di.unimi.it>
2020-11-27 10:58:15 +0100

    Improved configuration overriding and checking

--- scythe/confs.sh
+++ scythe/confs.sh
@@ -1,34 +1,59 @@
 # current version
 
-export SCYTHE_VERSION=1.3.2
+export SCYTHE_VERSION=1.3.3
 
-# tool dirs
+# user confs
 
 export CONFS_DIR="$(realpath -eL $SCYTHE_HOME/../confs)"
-export EXERCISES_DIR="$(realpath -eL $SCYTHE_HOME/../exercises)"
-export BACKUPS_DIR="$(realpath -eL $SCYTHE_HOME/../backups)"
+if [ ! -r "$CONFS_DIR/confs.sh" ]; then
+	echo "scythe: missing user confs '$CONFS_DIR/confs.sh'" >&2
+	exit 1
+fi
 
-# local dirs
+# defaults
+
+export EXERCISES_DIR="$SCYTHE_HOME/../exercises"
+export BACKUPS_DIR="$SCYTHE_HOME/../backups"
 
 export BASE_BUNDLE="$CONFS_DIR/basebundle"
 export EXERCISES="$CONFS_DIR/${SCYTHE_SESSION_ID}.txt"
 export REGISTERED_UIDS="$CONFS_DIR/${SCYTHE_SESSION_ID}.tsv"
+
 export TM_SETTINGS="$CONFS_DIR/${SCYTHE_SESSION_ID}-tm.py"
 export ST_SETTINGS="$CONFS_DIR/${SCYTHE_SESSION_ID}-st.py"
 
 export SCYTHE_BACKUPS="$BACKUPS_DIR/$SCYTHE_SESSION_ID"
 
-# user confs (here so that can override any of the above)
+# possibly ovverriden by user values
 
-if [ ! -r "$CONFS_DIR/confs.sh" ]; then
-	echo "scythe: missing user confs '$CONFS_DIR/confs.sh'" >&2
-	exit 1
-fi
 source "$CONFS_DIR/confs.sh"
+
+# fix missing SCYTHE_EDITOR
+
 if [ -z "$SCYTHE_EDITOR" ]; then
 	export SCYTHE_EDITOR="$EDITOR"
 fi
 
+# make all path absolute
+
+export EXERCISES_DIR="$(realpath -mL $EXERCISES_DIR)"
+if [ ! -d "$EXERCISES_DIR" ]; then
+	echo "scythe: missing exercises dir '$EXERCISES_DIR'" >&2
+	exit 1
+fi
+export BACKUPS_DIR="$(realpath -mL $BACKUPS_DIR)"
+if [ ! -d "$BACKUPS_DIR" ]; then
+	echo "scythe: missing backups dir '$BACKUPS_DIR'" >&2
+	exit 1
+fi
+
+export BASE_BUNDLE="$(realpath -mL $BASE_BUNDLE)"
+export EXERCISES="$(realpath -mL $EXERCISES)"
+export REGISTERED_UIDS="$(realpath -mL $REGISTERED_UIDS)"
+
+export TM_SETTINGS="$(realpath -mL $TM_SETTINGS)"
+export ST_SETTINGS="$(realpath -mL $ST_SETTINGS)"
+export SCYTHE_BACKUPS="$(realpath -mL $SCYTHE_BACKUPS)"
 
 # functions
 
